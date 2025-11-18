91e9dadbec341d0e1a526420906533ebed98cd46
Milozap <mzapolski96@gmail.com>
2025-11-18 18:13:02 +0100

    init

---
+++ src/test/java/com/mzap/apiservice/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzap.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
